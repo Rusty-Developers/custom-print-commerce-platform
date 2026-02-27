fd0645a1fee71c23833744a1c89f25c26eae98e4
rustydeveloper12430 <VERIFIED_EMAIL_OF_THAT_ACCOUNT>
2026-02-28 01:29:00 +0530

    Initial_Setup

---
+++ printcraft-backend/src/main/java/com/printcraft/printcraft_backend/PrintcraftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.printcraft.printcraft_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintcraftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintcraftBackendApplication.class, args);
+	}
+
+}
---
+++ printcraft-backend/src/test/java/com/printcraft/printcraft_backend/PrintcraftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printcraft.printcraft_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintcraftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
