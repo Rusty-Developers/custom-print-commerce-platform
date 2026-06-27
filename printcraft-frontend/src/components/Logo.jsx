33bf9127bf1ab64b3d69c86b3a4933f5732e5879
rustydeveloper12430 <VERIFIED_EMAIL_OF_THAT_ACCOUNT>
2026-06-27 19:30:44 +0530

    ""logo__navbar fixing"

--- printcraft-frontend/src/components/Logo.jsx
+++ printcraft-frontend/src/components/Logo.jsx
@@ -2,7 +2,7 @@ export default function Logo({ size = 28 }) {
   const height = Math.min(size * 1.8, 52)
   return (
     <img
-      src="/mk-group-logo.svg"
+      src="/mk-group-logo.jpg"
       alt="MK Group Printing"
       width={Math.round(height * 2.7)}
       height={height}
